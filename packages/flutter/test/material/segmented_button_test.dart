7cabe407d4edad7d37197b8449f60452ffdb366a
Taha Tesser <tessertaha@gmail.com>
2025-01-31 10:11:01 +0200

    add gesture teardown

--- packages/flutter/test/material/segmented_button_test.dart
+++ packages/flutter/test/material/segmented_button_test.dart
@@ -1268,6 +1268,7 @@ void main() {
     final TestGesture gesture = await tester.createGesture(kind: PointerDeviceKind.mouse);
     await gesture.addPointer();
     await gesture.moveTo(buttonLocation);
+    addTearDown(gesture.removePointer);
     await tester.pumpAndSettle();
 
     expect(find.byType(SegmentedButton<int>), paints..rrect(color: hoveredColor));
